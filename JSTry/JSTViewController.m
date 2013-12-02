e0fda89cae13be6e0316a1edf4dd1ecf1a26a460
Justin R. Miller <incanus@codesorcery.net>
2013-12-02 15:04:08 -0800

    initial cleanup

--- JSTry/JSTViewController.m
+++ JSTry/JSTViewController.m
@@ -17,13 +17,15 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    self.view.backgroundColor = [UIColor orangeColor];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+    NSLog(@"mem warning");
 }
 
 @end
